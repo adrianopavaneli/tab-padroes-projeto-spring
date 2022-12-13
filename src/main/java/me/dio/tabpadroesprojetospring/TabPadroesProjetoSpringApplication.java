9552b4972fdb6e9f058529d826e5e3577b9dc561
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-12-12 21:58:49 -0300

    first commit

---
+++ src/main/java/me/dio/tabpadroesprojetospring/TabPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package me.dio.tabpadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/tabpadroesprojetospring/TabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.tabpadroesprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
