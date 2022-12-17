4a54307d49ae683fa7ed5c1d688138d15b9a4fb3
adrianopavaneli <adriano.pavaneli.dev@gmail.com>
2022-12-17 13:55:02 -0300

    rest controller correcao

--- src/main/java/me/dio/tabpadroesprojetospring/controller/ClienteRestController.java
+++ src/main/java/me/dio/tabpadroesprojetospring/controller/ClienteRestController.java
@@ -25,7 +25,7 @@ public class ClienteRestController {
 	public ResponseEntity<Iterable<Cliente>> buscarTodos(){
 		return ResponseEntity.ok(clienteService.buscarTodos());		
 	}
-	@GetMapping("/{id")
+	@GetMapping("/{id}")
 	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
 		return ResponseEntity.ok(clienteService.buscarPorId(id));
 	}
@@ -34,12 +34,12 @@ public class ClienteRestController {
 		clienteService.inserir(cliente);
 		return ResponseEntity.ok(cliente);
 	}
-	@PutMapping("/{id")
+	@PutMapping("/{id}")
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
 		clienteService.atualizar(id, cliente);
 		return ResponseEntity.ok(cliente);
 	}
-	@DeleteMapping("/{id")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Cliente> deletar(@PathVariable Long id){
 		clienteService.deletar(id);
 		return ResponseEntity.ok().build();
